2d0b2d2afbfdc1fe1b86b2c3c90524737272ea08
Darius <dariushogan.edge.ai@gmail.com>
2024-12-04 04:04:14 +0000

    sv

---
+++ sha256/main.sv
@@ -0,0 +1,78 @@
+module password_cracker (
+    input  logic        clk,
+    input  logic        rst_n,
+    input  logic [255:0] target_hash,
+    input  logic [63:0]  target_salt,
+    output logic        found,
+    output logic [47:0] password    // 6 characters, 8 bits each
+);
+
+    // Constants
+    localparam CHARSET_SIZE = 62;
+    localparam PASSWORD_LENGTH = 6;
+    localparam HASH_SIZE = 256;
+    
+    // SHA-256 Constants
+    localparam logic [31:0] K [0:63] = '{
+        32'h428a2f98, 32'h71374491, 32'hb5c0fbcf, 32'he9b5dba5,
+        32'h3956c25b, 32'h59f111f1, 32'h923f82a4, 32'hab1c5ed5,
+        // ... (rest of the K constants)
+        32'h90befffa, 32'ha4506ceb, 32'hbef9a3f7, 32'hc67178f2
+    };
+        // Character set ROM
+    logic [7:0] charset_rom [0:61];
+    initial begin
+        charset_rom = '{
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+            "u", "v", "w", "x", "y", "z",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z",
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+        };
+    end
+
+        // Character set ROM
+    logic [7:0] charset_rom [0:61];
+    initial begin
+        charset_rom = '{
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+            "u", "v", "w", "x", "y", "z",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z",
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+        };
+    end
+
+        // Character set ROM
+    logic [7:0] charset_rom [0:61];
+    initial begin
+        charset_rom = '{
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+            "u", "v", "w", "x", "y", "z",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z",
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+        };
+    end
+
+        // Character set ROM
+    logic [7:0] charset_rom [0:61];
+    initial begin
+        charset_rom = '{
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+            "u", "v", "w", "x", "y", "z",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z",
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+        };
+    end
+
+    
